957ba247a41fe8f37f32b040f0e114051125f2bd
yalonsob <yalonsob@gmail.com>
2017-09-10 20:53:36 -0700

    Homework 2.3

---
+++ assets/js/hw2/hw23.js
@@ -0,0 +1,62 @@
+function countPositives(numbers) {
+    var positives = 0;
+    numbers.forEach(function (number) {
+        if(number > 0) {
+            positives++;
+        }
+    });
+    return positives;
+}
+
+function countNegatives(numbers) {
+    var negatives = 0;
+    numbers.forEach(function (number) {
+        if(number < 0) {
+            negatives++;
+        }
+    });
+    return negatives;
+}
+
+function countZeros(numbers) {
+    var zeros = 0;
+    numbers.forEach(function (number) {
+        if(number == 0) {
+            zeros++;
+        }
+    });
+    return zeros;
+}
+
+var $inputInteger1 = document.getElementById('integer-1');
+var $inputInteger2 = document.getElementById('integer-2');
+var $inputInteger3 = document.getElementById('integer-3');
+var $inputInteger4 = document.getElementById('integer-4');
+var $inputInteger5 = document.getElementById('integer-5');
+var $resultPositive = document.getElementById('result-positive');
+var $resultNegative = document.getElementById('result-negative');
+var $resultZeros = document.getElementById('result-zeros');
+var $btnShowResults = document.getElementById('btn-show-results');
+
+function showResults() {
+    var numbers = [
+        parseInt($inputInteger1.value),
+        parseInt($inputInteger2.value),
+        parseInt($inputInteger3.value),
+        parseInt($inputInteger4.value),
+        parseInt($inputInteger5.value)
+    ];
+    var positives = countPositives(numbers);
+    var negatives = countNegatives(numbers);
+    var zeros = countZeros(numbers);
+
+    $resultPositive.innerHTML = positives;
+    $resultNegative.innerHTML = negatives;
+    $resultZeros.innerHTML = zeros;
+}
+
+$btnShowResults.onclick = function (e) {
+    e.preventDefault();
+    showResults();
+};
+
